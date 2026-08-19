b068ced5196e068d475fff9da786fbc12b54a1dd
Gilmar Andrade <gilmar-andrade@outlook.com>
2026-08-19 12:00:48 -0300

    doc: update lexer comments

--- include/lexer.h
+++ include/lexer.h
@@ -25,6 +25,16 @@ typedef struct Lexer {
     size_t cursor;
 } Lexer;
 
+/**
+ * The Lexer tokenizes a source string
+ * 
+ * Iterator / Stream Pattern (Pull-Based): The parser acts as the driver, explicitly asking the lexer for the "next token" only when needed via lexer_next_token().
+ * This minimizes memory overhead because tokens are processed lazily.
+ * 
+ * Lexers map transitions character-by-character using deterministic finite automata (state machines).
+ * The lexer buffers characters to look one or more steps ahead before deciding on a token type.
+ * 
+ */
 Token lexer_next_token(Lexer* lexer);
 
 bool is_digit(char c);
