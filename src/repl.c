01b3a9485e9366cf2a65efe1a779bc5de1b667f9
Gilmar Andrade <gilmar-andrade@outlook.com>
2026-08-31 19:07:05 -0300

    feat: REPL fgest input

--- src/repl.c
+++ src/repl.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include "repl.h"
+#include "lexer.h"
 
 void repl_start(void) {
-    printf("MathLang REPL (placeholder)\n");
-    printf("Type 'quit' to exit.\n");
+    char input[100];
+    printf("MathLang REPL\n");
+    // printf("Type 'quit' to exit.\n");
+    fgets(input, sizeof(input), stdin);
     // Actual loop comes later.
+
+    Lexer lexer = {
+        .source = input,
+        .cursor = 0,
+    };
+
+    while(true) {
+        Token token = lexer_next_token(&lexer);
+        printf("%s: <%d>\n", get_token_type_name(token.type), token.value);
+
+        // TODO push to array
+        if(token.type == TOKEN_END) {
+            break;
+        }
+    }
 }
