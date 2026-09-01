e453bbd38740d76ba641480f775cd3d4c2fecbe6
Gilmar Andrade <gilmar-andrade@outlook.com>
2026-09-01 16:55:15 -0300

    feat: repl - push lexed tokens to TokenList

--- src/repl.c
+++ src/repl.c
@@ -3,6 +3,8 @@
 #include "lexer.h"
 
 void repl_start(void) {
+    Arena *a = arena_alloc(1024);
+
     char input[100];
     printf("MathLang REPL\n");
     // printf("Type 'quit' to exit.\n");
@@ -14,13 +16,19 @@ void repl_start(void) {
         .cursor = 0,
     };
 
+    TokenList* token_list = create_token_list(a, 10);
+
+    // Lexer step
     while(true) {
         Token token = lexer_next_token(&lexer);
         printf("%s: <%d>\n", get_token_type_name(token.type), token.value);
 
-        // TODO push to array
+        token_list_push(token_list, token);
+
         if(token.type == TOKEN_END) {
             break;
         }
     }
+
+    printf("found %ld tokens\n", token_list->length);
 }
